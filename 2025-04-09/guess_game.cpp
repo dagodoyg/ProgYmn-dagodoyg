db4582698cfc27a055e59635bfe5c6aea670f42d
dagodoyg <dagodoyg@unal.edu.co>
2025-04-09 08:33:00 -0500

    añadí el ciclo for

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -3,15 +3,22 @@
 int main(void){
     const int numero=10; //const lo hace de solo lectura, no puede cambiar
     int lambda;
+    for ( ; ; ){
     std::cout << "Adivina el numero entre 1 y 100:\n";
     std::cin >> lambda;
         if (lambda==numero)
         {
             std::cout << "Adivinaste\n";
+            break;
         } else if (lambda>numero) {
             std::cout << "Te pasaste\n";
+        //} 
+        /*else if ((lambda>100)||(lambda<1)) {
+            std::cout << "Numero invalido":
+        }*/
         } else {
             std::cout << "Te quedaste corto\n";            
         }
+    }
     return 0;
 }

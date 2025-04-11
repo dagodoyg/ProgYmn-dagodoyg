f873b645be545142f07db869f5941f5f249428ae
dagodoyg <dagodoyg@unal.edu.co>
2025-04-11 08:35:43 -0500

    se movió todo a una función y se implementó detección de errores

--- 2025-04-11/guess_game.cpp
+++ 2025-04-11/guess_game.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
+#include <limits>
 //para adivinar un número
-int main(void){
+
+//declaración
+void play(void); 
+
+int main (void){
+    play();
+    return 0;
+}
+
+//implementación
+void play(void){
     const int numero=10; //const lo hace de solo lectura, no puede cambiar
+    const int liminf=1;
+    const int limsup=100;
     int lambda=numero+1;
-    for ( ; ; ){
-    std::cout << "Adivina el numero entre 1 y 100:\n";
+    while(lambda!=numero){
+    std::cout << "Adivina el numero entre " << liminf << " y " << limsup << "\n";
     std::cin >> lambda;
         if (lambda==numero)
         {
             std::cout << "Adivinaste\n";
             break;
-        } else if ((lambda>100)||(lambda<1)) {
-            std::cout << "Numero invalido\n";
+
+        } else if ( std::cin.fail()){ //validación de la entrada
+            std::cout << "No es un numero, ingrese una respuesta valida\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        } else if ((lambda>limsup)||(lambda<liminf)) { //validación del rango
+            std::cout << "Numero fuera del limite\n";
+
         } else if (lambda>numero) {
             std::cout << "Te pasaste\n";
+
         } else {
             std::cout << "Te quedaste corto\n";            
         }
     }
-    return 0;
 }

c7ddaa400e109c37b4eabd4d7fab2463ee91159b
dagodoyg <dagodoyg@unal.edu.co>
2025-04-23 08:35:56 -0500

    suma de primos y cousin primes

---
+++ 2025-04-23/cousin_primes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+
+bool is_prime(long n);
+
+int main(void){
+    long lim_inf {300};
+    long lim_sup {400-4};
+    for (long ii=lim_inf;ii<=lim_sup;ii++){
+        if( is_prime(ii) & is_prime(ii+4)){
+            std::cout << "\033[32m( " << ii << " , " << ii+4 << " ) son cousin primes\033[0m \n" ;
+        }
+    };
+    return 0;
+}
+
+
+
+bool is_prime(long n){
+ if(n<=1){ return false; }
+ for(long ii=2;(ii*ii)<=n;ii++){
+    if( n%ii==0 ){
+        return false;
+        } 
+    }
+    return true;
+}
+
--- 2025-04-23/primos2.cpp
+++ 2025-04-23/primos2.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
 
-bool is_prime(int n);
-
+bool is_prime(long n);
 
 int main(void){
-    int lim_inf {1};
-    int lim_sup {150};
-    std::cout << "\033[32mLos numeros primos desde "<<lim_inf<<" hasta "<<lim_sup<< " son\033[0m\n" ;
-    for (int ii=lim_inf;ii<=lim_sup;ii++){
+    long lim_inf {500};
+    long lim_sup {1234};
+    long sum_primes {0};
+    std::cout << "\033[32mLos numeros primos desde "<<lim_inf<<" hasta "<<lim_sup<< " son\033[35m\n" ;
+    for (long ii=lim_inf;ii<=lim_sup;ii++){
         if( is_prime(ii) ){
-            std::cout << "\033[35m" << ii << "\033[0m\n" ;
+            std::cout << ii << "\n" ;
+            sum_primes=sum_primes+ii;
         }
     };
+    std::cout << "\033[32mLa suma de los numeros primos entre " <<lim_inf<<" y "<<lim_sup << " es \033[35m"<< sum_primes<< "\033[0m\n";
     return 0;
 }
 
 
 
-bool is_prime(int n){
+bool is_prime(long n){
  if(n<=1){ return false; }
- for(int ii=2;(ii*ii)<=n;ii++){
+ for(long ii=2;(ii*ii)<=n;ii++){
     if( n%ii==0 ){
         return false;
         } 

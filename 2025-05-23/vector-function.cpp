015628f65383cb286c91e10a20704edeab0d7cb7
dagodoyg <dagodoyg@unal.edu.co>
2025-05-23 07:37:12 -0500

    uso de algoritmos standard

---
+++ 2025-05-23/vector-function.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <numeric>
+
+double average(const std::vector<double> & x);
+
+int main(int argc , char **argv)
+{
+    // declare the vector
+    std::vector<double> data; 
+
+    // read the size from the command line
+    const int N = std::stoi(argv[1]); 
+
+    // resize the vector
+    std::cout << "Pidiendo memoria ...\n";
+    data.resize(N, 0);
+
+    auto print =[](double x){
+        std::cout << x << "\n";
+    };
+    std::for_each(data.begin(), data.end(), print);
+
+    // initialize the data
+    std::cout << "Iniciando datos ...\n";
+
+    int ii{0};
+    auto fill = [&ii](double & x){ x=ii; ii++; };
+    std::for_each(data.begin(), data.end(), fill);
+    std::for_each(data.begin(), data.end(), print);
+
+
+    // compute the average
+    std::cout << "Calculando promedio ...\n";
+    std::cout.precision(16);
+    std::cout.setf(std::ios::scientific);
+    std::cout << std::accumulate(data.begin(),data.end(),0.0)/data.size() << "\n";
+
+    return 0;
+}    
+

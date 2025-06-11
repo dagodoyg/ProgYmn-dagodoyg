e106571ddeef29b319850f8ae52ea0342c97771a
dagodoyg <dagodoyg@unal.edu.co>
2025-06-10 20:27:53 -0500

    nada

--- 2025-06-06/valarray.cpp
+++ 2025-06-06/valarray.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <valarray>
-#include <cmath>
 
-int main(void){
-    std::valarray<int> v = {1,2,3,4,5,6,7,8,9,10};
+int main()
+{
+  std::valarray<int> data = {0,1,2,3,4,5,6,7,8,9};
 
-    double suma=v.sum();
+  std::cout << "Initial valarray: ";
+  for(int n: data) std::cout << n << ' ';
+  std::cout << '\n';
 
-    std::cout << suma << "\n";
+  data[data > 5] = -1; // valarray<bool> overload of operator[]
+  // the type of data>5 is std::valarray<bool>
+  // the type of data[data>5] is std::mask_array<int>
 
-    return 0;    
+  std::cout << "After v[v>5]=-1:  \n";
+  for(std::size_t n = 0; n < data.size(); ++n)
+    std::cout << data[n] << ' ';  // regular operator[]
+  std::cout << '\n';
 }

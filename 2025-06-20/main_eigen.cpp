7d4f0dded0cf3f9fc13c97dea1abc8300042254a
dagodoyg <dagodoyg@unal.edu.co>
2025-06-20 08:57:03 -0500

    main eigen

--- 2025-06-20/main_eigen.cpp
+++ 2025-06-20/main_eigen.cpp
@@ -4,18 +4,18 @@
 
 int main(int argc, char** argv)
 {
-   int N = std::stoi(argv[1]);
+    int N = std::stoi(argv[2]);
+    srand(std::stoi(argv[1]));
+    Eigen::Matrix3d A = Eigen::Matrix3d::Random(N,N);
+    Eigen::Vector3d b = Eigen::Vector3d::Random(N);
+    //std::cout.precision(16);
+    //std::cout.setf(std::ios::scientific); 
 
-   Eigen::MatrixXd A = Eigen::MatrixXd::Random(N,N);
-   Eigen::Vector3d b = Eigen::Vector3d::Random(N);
-   //std::cout.precision(16);
-   //std::cout.setf(std::ios::scientific);
+    std::cout << "Here is the matrix A:\n" << A << std::endl;
+    std::cout << "Here is the vector b:\n" << b << std::endl;
+    Eigen::Vector3d x = A.colPivHouseholderQr().solve(b);
+    std::cout << "The solution is:\n" << x << std::endl;
+    std::cout << (A*x - b).norm() << "\n";  
 
-   std::cout << "Here is the matrix A:\n" << A << std::endl;
-   std::cout << "Here is the vector b:\n" << b << std::endl;
-   Eigen::Vector3d x = A.colPivHouseholderQr().solve(b);
-   std::cout << "The solution is:\n" << x << std::endl;
-   std::cout << (A*x - b).norm() << "\n";
-
-   return 0;
+    return 0;
 }

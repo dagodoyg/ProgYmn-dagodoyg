0356d15001004e6da76510b7493dbc692ce56bf5
davidgggamerYT <davidgodoygarzon@gmail.com>
2025-06-29 19:08:04 -0500

    harmonic oscilator solve

--- Casa/Oscilador_armonico/heunmonic.cpp
+++ Casa/Oscilador_armonico/heunmonic.cpp
@@ -13,6 +13,7 @@ void solve_heun(deriv_t fderiv, system_t & s, double tinit, double tend, double 
     system_t k2(s.size());
 
     std::ofstream heun("output-heun.txt");
+    heun << "t\tx\tv\n"; 
 
     // time loop
     for(double t = tinit; t <= tend; t = t + dt) { // NOTE: Last time step not necessarily tf
@@ -23,7 +24,7 @@ void solve_heun(deriv_t fderiv, system_t & s, double tinit, double tend, double 
         // compute new state. NOTE: Not using components, assuming valarray or similar
         s = s + dt*(k1 + k2)/2.0; // Heun's method
 
-        heun << t << "\t" << s[0] << "\n";
+        heun << t << "\t" << s[0] << "\t" << s[1] << "\n";
     }
 
     heun.close();
@@ -36,6 +37,7 @@ void solve_euler(deriv_t fderiv, system_t & s, double tinit, double tend, double
     system_t dsdt(s.size());
 
     std::ofstream euler("output-euler.txt");
+    euler << "t\tx\tv\n"; 
 
     // time loop
     for(double t = tinit; t <= tend; t = t + dt) { // NOTE: Last time step not necessarily tf
@@ -45,7 +47,7 @@ void solve_euler(deriv_t fderiv, system_t & s, double tinit, double tend, double
         // compute new state. NOTE: Not using components, assuming valarray or similar
         s = s + dt*dsdt; // Euler
 
-        euler << t << "\t" << s[0] << "\n";
+        euler << t << "\t" << s[0] << "\t" << s[1] << "\n";
       }
 
       euler.close();
@@ -56,16 +58,21 @@ int main(int argc, char** argv){
     double t0 = std::stod(argv[2]);
     double tf = std::stod(argv[3]);
     double w = std::stod(argv[4]);
+    state_t y(2);
 
     auto fun = [w](const state_t & y, state_t & dsdt, double t){
-        dsdt = -1.0*w*w*y;
+        dsdt[0] = y[1];  //Position derivative
+        dsdt[1] = -1.0*w*w*y[0];  //Speed derivative
     };
 
-    state_t y = {1};
+    auto initial_con = [](state_t & y){
+        y = {1,0};
+    };
 
+    initial_con(y);
     solve_euler(fun,y,t0,tf,dt);
 
-    y = {1};
+    initial_con(y);
     solve_heun(fun,y,t0,tf,dt);
 
     return 0;

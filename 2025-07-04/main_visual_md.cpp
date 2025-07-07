00a51388694feaa4dc1ce6d1c6c6aee6af1813aa
davidgggamerYT <davidgodoygarzon@gmail.com>
2025-07-07 10:45:44 -0500

    200 particles random size

--- 2025-07-04/main_visual_md.cpp
+++ 2025-07-04/main_visual_md.cpp
@@ -5,14 +5,16 @@
 #include "collider.h"
 #include "boundary.h"
 #include <vector>
+#include <random>
+#include <algorithm>
 
 #include <SFML/Graphics.hpp>
 
-void initial_conditions(std::vector<Particle> & particles);
+void initial_conditions(std::vector<Particle> & particles, double min_p_rad, double max_p_rad);
 
 int main(int argc, char **argv) {
-  std::vector<Particle> bodies(1); // only one particle for now
-
+  std::vector<Particle> bodies; // only one particle for now
+  bodies.resize(200);
   // parameters
   std::map<std::string, double> p;
   p["T0"] = 0.0;
@@ -20,10 +22,12 @@ int main(int argc, char **argv) {
   p["DT"] = 0.01;
   p["G"] = 0.0; //-9.81;
   p["K"] = 200.543;
+  p["MIN_prad"] = 0.05;
+  p["MAX_prad"] = 0.1;
 
   // window
-  const int WIDTH = 800, HEIGHT = 600;
-  const float METERS_TO_PIXELS = 100.0f; // 200 pixels per meter
+  const int WIDTH = 1800, HEIGHT = 1000;
+  const float METERS_TO_PIXELS = 75.0f; // 200 pixels per meter
   
   // Force collider
   Collider collider(p);
@@ -32,10 +36,10 @@ int main(int argc, char **argv) {
   TimeIntegrator integrator(p["DT"]);
 
   // Boundary conditions
-  Boundary bc(2.345, 0.0, 0.0, 0.0, 1.0); // RMAX, CX, CY, CZ, EN
+  Boundary bc(6, 0.0, 0.0, 0.0, 1.0); // RMAX, CX, CY, CZ, EN
 
   // initial conditions and properties
-  initial_conditions(bodies);
+  initial_conditions(bodies,p["MIN_prad"],p["MAX_prad"]);
   collider.computeForces(bodies); // force at t = 0
   integrator.startIntegration(bodies); // start integration algorithm
   std::cout << p["T0"] << "\t";
@@ -43,11 +47,9 @@ int main(int argc, char **argv) {
   // --- VISUALIZATION SETUP ---
   sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "C++ Simulation & Visualization");
   window.setFramerateLimit(120);
-   std::cout << "a "<< "\n";
   sf::CircleShape particle_shape(bodies[0].rad);
   particle_shape.setFillColor(sf::Color::Cyan);
   particle_shape.setOrigin(0, 0);
-  std::cout << true << std::endl;
 
   sf::CircleShape circle(2.345*METERS_TO_PIXELS); // Radius of 50 pixels
   circle.setFillColor(sf::Color::Green);
@@ -78,8 +80,7 @@ int main(int argc, char **argv) {
       // Center the origin so setPosition() refers to the center of the circle
       particle_shape.setOrigin(pixel_radius, pixel_radius);
 
-      sf::Uint8 speed = std::sqrt((p.V*p.V).sum())*255/5;
-      sf::Color particleColor(0,0,0);
+      sf::Color particleColor(p.rad*255/0.5,p.rad*255/0.5,p.rad*255/0.5);
       particle_shape.setFillColor(particleColor);
 
       // Convert world coordinates to screen coordinates
@@ -103,11 +104,21 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-void initial_conditions(std::vector<Particle> & particles)
-{
-  particles[0].R[2] = 0.987;  // z is upwards, x to the right
-  particles[0].V[1] = 4.9876;//12.987; // z is upwards, x to the right
-  particles[0].V[2] = 0.0; //4.9876; //3.987; // z is upwards, x to the right
-  particles[0].rad  = 0.103;
-  particles[0].mass = 0.337;
+void initial_conditions(std::vector<Particle> & particles, double min_p_rad, double max_p_rad){
+
+  std::random_device random;
+  std::mt19937 gen(random());
+  std::uniform_real_distribution<> distrib_R(-1.0, 1.0);
+  std::uniform_real_distribution<> distrib_V(-5.0, 5.0);
+  std::uniform_real_distribution<> distrib_rad(min_p_rad,max_p_rad);
+  for (int idx{0}; idx < particles.size(); idx++){
+    particles[idx].R[0] = distrib_R(gen);
+    particles[idx].R[1] = distrib_R(gen);
+    particles[idx].R[2] = distrib_R(gen);
+    particles[idx].V[0] = distrib_V(gen);
+    particles[idx].V[1] = distrib_V(gen);
+    particles[idx].V[2] = distrib_V(gen);
+    particles[idx].rad  = distrib_rad(gen);
+    particles[idx].mass = 0.337;
+  }
 }
